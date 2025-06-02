a14fea9bddf592da2b9cfab13e6a9807c0d358f5
earutyunyan15 <arutunangevorg366@gmail.com>
2025-06-02 11:12:07 +0800

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,85 +1,94 @@
 from pygame import *
 from random import randint
 
-window = display.set_mode((700, 500))
+weidth, height = 700, 500
+window = display.set_mode((weidth, height))
 display.set_caption('Turtle')
-game = True
-finish = False
 font.init()
 font1 = font.SysFont("Arial", 100)
-weidth = 700
-height = 500
+font2 = font.SysFont("Arial", 36)
+clock = time.Clock()
+speed = 15
+background = transform.scale(image.load('snake_back.jpg'), (weidth, height))
 x = randint(0, weidth - 35)
 y = randint(0, height - 35)
-snake_list = [(x,y)]
-snake_segments = []
-lenght = 1
-x1 = 0
-y1 = 0
+snake_list = [(x, y)]
+snake_length = 1
 apples = 0
-font2 = font.SysFont('Arial', 36)
-speed = 15
-clock = time.Clock()
-background = transform.scale(image.load('snake_back.jpg'),(weidth,height))
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (35, 35))
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Snake(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__(player_image, player_x, player_y, player_speed, size_x, size_y)
+        self.dir_x = player_speed
+        self.dir_y = 0
+
     def update(self):
-        x1 = 0
-        y1 = 0
-        k_p = key.get_pressed()
-        if k_p[K_LEFT] and self.rect.x > 0:
-            x1 = -self.speed
-            y1 = 0
-        elif k_p[K_RIGHT] and self.rect.x < 665:
-            x1 = self.speed
-            y1 = 0
-        elif k_p[K_UP] and self.rect.y > 0:
-            y1 = -self.speed
-            x1 = 0
-        elif k_p[K_DOWN] and self.rect.y < 465:
-            y1 = self.speed
-            x1 = 0
-        self.rect.x += x1
-        self.rect.y += y1
-    def grow_up(self):
-        if lenght < 
+        self.rect.x += self.dir_x
+        self.rect.y += self.dir_y
+
 class Food(GameSprite):
-    def update(self):
-        self.rect.x = randint(0, weidth - 35) 
-        self.rect.y = randint(0, height - 35) 
-player = Snake('snake.png', x, y, speed, 35, 35)
-apple = Food('apple_new.jpg', randint(0, weidth - 35), randint(0, height - 35), 0, 20, 20)
+    def __init__(self, player_image, player_x, player_y, size_x, size_y):
+        super().__init__(player_image, player_x, player_y, 0, size_x, size_y)
+
+    def relocate(self):
+        self.rect.x = randint(0, weidth - 35)
+        self.rect.y = randint(0, height - 35)
+
+head = Snake('snake.png', x, y, speed, 35, 35)
+apple = Food('apple_supernew.png', randint(0, weidth - 35), randint(0, height - 35), 50, 50)
+game = True
+finish = False
+
 while game:
-    loser = font1.render('Проигрыш!', 1, (255, 0, 0))
-    counter = font2.render('Счёт:' + str(apples), 1, (255, 255, 255))
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if e.key == K_LEFT and head.dir_x == 0:
+                head.dir_x = -speed
+                head.dir_y = 0
+            elif e.key == K_RIGHT and head.dir_x == 0:
+                head.dir_x = speed
+                head.dir_y = 0
+            elif e.key == K_UP and head.dir_y == 0:
+                head.dir_x = 0
+                head.dir_y = -speed
+            elif e.key == K_DOWN and head.dir_y == 0:
+                head.dir_x = 0
+                head.dir_y = speed
+
     if not finish:
-        window.blit(background, (0, 0))   
-        apple.reset()
-        player.reset()
-        player.update()
-        player.grow_up()
-        player1.reset()
-        player1.update()
-        window.blit(counter, (10, 10))
-        if sprite.collide_rect(player, apple):
-            apple.update()
-            apples += 1
-            snake_list.append((player.rect.x, player.rect.y))
-        if player.rect.x == 660 or player.rect.y == 460 or player.rect.x == 0 or player.rect.y == 0:
-            window.blit(loser, (175, 175))
+        window.blit(background, (0, 0))
+        head.update()
+        new_head_pos = (head.rect.x, head.rect.y)
+        snake_list.append(new_head_pos)
+        if len(snake_list) > snake_length:
+            del snake_list[0]
+        for pos in snake_list:
+            window.blit(transform.scale(image.load('snake.png'), (35, 35)), pos)
+        if head.rect.x < 0 or head.rect.x > weidth - 35 or head.rect.y < 0 or head.rect.y > height - 35:
+            loser = font1.render('Проигрыш!', True, (255, 0, 0))
+            window.blit(loser, (weidth // 2 - loser.get_width() // 2, height // 2 - loser.get_height() // 2))
             finish = True
+        apple.reset()
+        if sprite.collide_rect(head, apple):
+            apples += 1
+            snake_length += 1
+            apple.relocate()
+        counter = font2.render(f'Счёт: {apples}', True, (255, 255, 255))
+        window.blit(counter, (10, 10))
+
     display.update()
     clock.tick(speed)
